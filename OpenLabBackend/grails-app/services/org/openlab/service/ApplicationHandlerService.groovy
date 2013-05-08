d1b4d2be4a3afcf727799f3a9bc26847a9ff6a3c
Markus List <mlist@health.sdu.dk>
2013-05-08 10:20:49 +0200

    Added GPL v3 license header to all files, as well as a license.txt located at the project root directory.

--- OpenLabBackend/grails-app/services/org/openlab/service/ApplicationHandlerService.groovy
+++ OpenLabBackend/grails-app/services/org/openlab/service/ApplicationHandlerService.groovy
@@ -1,3 +1,32 @@
+/*
+ * Copyright (C) 2013 
+ * Center for Excellence in Nanomedicine (NanoCAN)
+ * Molecular Oncology
+ * University of Southern Denmark
+ * ###############################################
+ * Written by:	Markus List
+ * Contact: 	mlist'at'health'.'sdu'.'dk
+ * Web:		http://www.nanocan.org
+ * ###########################################################################
+ *	
+ *	This file is part of OpenLabFramework.
+ *
+ *  OpenLabFramework is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *   along with this program. It can be found at the root of the project page.
+ *	If not, see <http://www.gnu.org/licenses/>.
+ *
+ * ############################################################################
+ */
 package org.openlab.service;
 
 import java.util.List;
